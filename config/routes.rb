8fc8cbdce245f730c26d1099f7f44fd3639ddb18
Charlie Chai <charlie.chai@mail.utoronto.ca>
2019-11-11 12:34:56 -0500

    Skeleton code for notes functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'note/index'
     devise_for :users
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     resources :posts
     get "about", to:"pages#about"
     root 'posts#index'
+    
+    resources :notes
 end
