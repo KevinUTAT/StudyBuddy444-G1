dc8f0adec927d537eba4c3f5a23f04e3ce6b0af4
Charlie Chai <charlie.chai@mail.utoronto.ca>
2019-11-20 14:08:55 -0500

    Added sorting to notes page

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -1,2 +1,8 @@
 module NotesHelper
+    #sorting
+    def togglesort(column, title = nil)
+        title = title || column.titleize
+        direction = (column == params[:sort] && params[:direction] == "asc") ? "desc" : "asc"
+        link_to title, :sort => column, :direction => direction
+    end
 end
