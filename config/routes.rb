c0a84f0c2d9d88ca5a33a902d401912548244fa2
ander601 <jamie.anderson@mail.utoronto.ca>
2019-11-20 15:34:51 -0500

    added edit user page & modified nav bar to include settings dropdown

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,16 @@ Rails.application.routes.draw do
   get 'groups/new'
   get 'groups/edit'
   get 'dashboard/index'
-    get 'home/index'
-    devise_for :users, controllers: {
-        sessions: 'users/sessions'
-    }
-    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    resources :posts
-    root 'home#index'
-    
-    resources :notes
-    get 'note/index'
-    get 'like' => 'notes#like'
-    get 'unlike' => 'notes#unlike'
+  get 'home/index'
+  devise_for :users, controllers: {
+      sessions: 'users/sessions'
+  }
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts
+  root 'home#index'
+  
+  resources :notes
+  get 'note/index'
+  get 'like' => 'notes#like'
+  get 'unlike' => 'notes#unlike'
 end
