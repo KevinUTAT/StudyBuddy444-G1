e0a22a66feb4741380d40b942d362e6a3bd5262c
wheresahmed <ahmedomar13x@gmail.com>
2019-11-22 14:49:00 -0500

    add crud functionality to groups & enable joining

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
   def index
-    
+    @groups = Group.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,20 @@
 Rails.application.routes.draw do
-  get 'groups/index'
-  get 'groups/show'
-  get 'groups/new'
-  get 'groups/edit'
-  get 'dashboard/index'
-  get 'home/index'
-  devise_for :users, controllers: {
-      sessions: 'users/sessions'
-  }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :posts
-  root 'home#index'
-  
-  resources :notes
-  get 'note/index'
-  get 'like' => 'notes#like'
-  get 'unlike' => 'notes#unlike'
+    resources :groups do
+        collection do
+            post 'join'
+        end
+    end
+    get 'dashboard/index'
+    get 'home/index'
+    devise_for :users, controllers: {
+        sessions: 'users/sessions'
+    }
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    resources :posts
+    root 'home#index'
+    
+    resources :notes
+    get 'note/index'
+    get 'like' => 'notes#like'
+    get 'unlike' => 'notes#unlike'
 end
