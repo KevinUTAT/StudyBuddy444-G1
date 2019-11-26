5689561979cd7921bea3c290e8b979ab7efeaedd
ander601 <jamie.anderson@mail.utoronto.ca>
2019-11-25 23:55:46 -0500

    added buddies page which displays all users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  
+    match '/users',   to: 'users#index',   via: 'get'
+    match '/users/:id',     to: 'users#show',       via: 'get'
+
     resources :groups do
         collection do
             post 'join'
