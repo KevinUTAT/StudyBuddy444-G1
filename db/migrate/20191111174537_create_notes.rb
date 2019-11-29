5d7b7a3dff2f4df06d93af85ad634b6f2fa1e995
Charlie Chai <charlie.chai@mail.utoronto.ca>
2019-11-29 14:25:26 -0500

    Added professor column to notes

--- db/migrate/20191111174537_create_notes.rb
+++ db/migrate/20191111174537_create_notes.rb
@@ -3,6 +3,7 @@ class CreateNotes < ActiveRecord::Migration[6.0]
     create_table :notes do |t|
         t.string :title
         t.string :course
+        t.string :prof
         t.text :content
         
         t.timestamps
