f6314ea7ff3d65c0a36281788d712f0980dc5260
wheresahmed <a.abdulkadir@mail.utoronto.ca>
2019-12-02 12:31:58 -0500

    skeleton and routes for home views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,20 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def aboutus
+  end
+
+  def tos
+  end
+
+  def privacy
+  end
+
+  def map
+  end
+
+  def faq
+  end 
+  
 end
