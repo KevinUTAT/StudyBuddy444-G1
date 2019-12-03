8eb82554614cdce7289c6da13ca0174b2bcc504c
ander601 <jamie.anderson@mail.utoronto.ca>
2019-12-03 04:59:58 -0500

    fixed create page bug

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -53,7 +53,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # The path used after sign up.
   def after_sign_up_path_for(resource)
     #super(resource)
-    new_page_path(resource)
+    resource.page = Page.create(name: resource.name)
   end
 
   # The path used after sign up for inactive accounts.
