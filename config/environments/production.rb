aa41ebd86d8a2c5c63f623511ad4557abc9859b1
ander601 <jamie.anderson@mail.utoronto.ca>
2019-12-02 21:30:25 -0500

    trying to implement forgot password on heroku 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,6 +90,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'https://stormy-depths-05731.herokuapp.com/' }
+  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.smtp_settings = {
     :address        =>  'smtp.sendgrid.net',
