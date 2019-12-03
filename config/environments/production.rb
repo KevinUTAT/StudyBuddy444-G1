b373c5e120ea923c91d2da70580eb101b907b129
ander601 <jamie.anderson@mail.utoronto.ca>
2019-12-02 21:54:12 -0500

    trying to implement forgot password on heroku 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://stormy-depths-05731.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://stormy-depths-05731.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.smtp_settings = {
