80561069a4a1453a53e3b2280ea79b77df5bd0a9
ander601 <jamie.anderson@mail.utoronto.ca>
2019-12-02 23:13:27 -0500

    trying to implement forgot password on heroku 9

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,10 +32,24 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.smtp_settings = { 
+    :user_name => Rails.application.credentials.sendgrid[:userName],
+    :password => Rails.application.credentials.sendgrid[:password],
+    :domain => 'stormy-depths-05731.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+   
+   
+   
+     }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -60,20 +74,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = { 
-    :user_name => Rails.application.credentials.sendgrid[:userName],
-    :password => Rails.application.credentials.sendgrid[:password],
-    :domain => 'https://stormy-depths-05731.herokuapp.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-   
-   
-   
-     }
+ 
 
 end
